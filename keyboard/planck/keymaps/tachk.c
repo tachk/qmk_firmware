58ebf96948bb60cd1d0bfbe3da00dc62e05451c3
tachk <k@tachk.net>
2016-02-23 21:21:14 -0800

    Updated keymapping after more use of the keyboard.

--- keyboard/planck/keymaps/tachk.c
+++ keyboard/planck/keymaps/tachk.c
@@ -20,34 +20,34 @@ as the default key layout. NOTE: TG() "Only works upwards in the layer stack." *
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_LW] = { /* LAYER 2: FUNCTIONS 'N' MACROS LAYER */
-      {KC_MUTE,     KC_VOLD,    KC_VOLU, M(1),    KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_DEL },
-      {KC_NO,       KC_UNDS,    KC_EQL,  KC_LCBR, KC_RCBR, KC_PLUS, KC_MINS, KC_LBRC, KC_RBRC, KC_NO,   KC_NO,   KC_CAPS},
+      {KC_MUTE,     KC_VOLD,    KC_VOLU, M(1),    KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_UNDS, KC_EQL,  KC_DEL },
+      {KC_NO,       KC_LCBR,    KC_RCBR, KC_NO,   KC_PLUS, KC_NO,   KC_NO,   KC_MINS, KC_NO,   KC_LBRC, KC_RBRC, KC_CAPS},
       {KC_F1,       KC_F2,      KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12 },
-      {KC_NO,       KC_NO,      KC_NO,   KC_NO,   KC_TRNS, KC_NO,   KC_NO,   TG(4),   KC_HOME, KC_NO,   KC_NO,   KC_END }
+      {KC_NO,       M(0),       KC_NO,   KC_NO,   KC_TRNS, KC_NO,   KC_NO,   KC_NO,   KC_HOME, KC_NO,   KC_NO,   KC_END }
     },
     [_BS] = { /* LAYER 0: QWERTY LAYER */
       {KC_ESC,      KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC},
       {KC_TAB,      KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT},
       {KC_LSFT,     KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT },
-      {KC_LCTL,     M(0),    KC_LGUI, KC_LALT, MO(2),   KC_SPC,  KC_SPC,  TG(1),   KC_LEFT, KC_UP,   KC_DOWN, KC_RGHT}
+      {KC_LCTL,     TG(3),   KC_LGUI, KC_LALT, MO(2),   KC_SPC,  KC_SPC,  MO(1),   KC_LEFT, KC_UP,   KC_DOWN, KC_RGHT}
     },
     [_RS] = { /* LAYER 1: NUM, BRACKETS, ETC. LAYER */
       {KC_GRV,   KC_1,                   KC_2,           KC_3,       KC_4,       KC_5,    KC_6,    KC_7,     KC_8,    KC_9,    KC_0,    KC_BSPC },
-      {KC_NO,    KC_NO,                  KC_NO,          KC_NO,      KC_NO,      KC_PLUS, KC_MINS, KC_NO,    KC_NO,   KC_NO,   KC_NO,   KC_BSLS },
+      {KC_NO,    KC_NO,                  KC_NO,          KC_NO,      KC_PLUS,    KC_NO,   KC_NO,   KC_MINS,  KC_NO,   KC_NO,   KC_NO,   KC_BSLS },
       {KC_LSFT,  LCTL(LSFT(KC_TAB)),     LCTL(KC_TAB),   KC_COPY,    KC_PASTE,   KC_NO,   KC_NO,   KC_NO,    KC_NO,   KC_NO,   KC_NO,   KC_ENT  },
-      {KC_NO,    KC_NO,                  KC_NO,          KC_NO,      TG(1),      KC_SPC,  KC_SPC,  TG(3),    KC_MS_L, KC_MS_U, KC_MS_D, KC_MS_R }
+      {KC_NO,    KC_NO,                  KC_NO,          KC_NO,      KC_NO,      KC_SPC,  KC_SPC,  KC_TRNS,  KC_MS_L, KC_MS_U, KC_MS_D, KC_MS_R }
     },
     [_CM] = { /* LAYER 3: COLEMAK ALPHAS LAYER */
       {KC_ESC,      KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,    KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_BSPC},
       {KC_TAB,      KC_A,    KC_R,    KC_S,    KC_T,    KC_D,    KC_H,    KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT},
       {KC_LSFT,     KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT },
-      {KC_LCTL,     M(0),    KC_LGUI, KC_LALT, MO(2),   KC_SPC,  KC_SPC,  TG(3),   KC_LEFT, KC_UP,   KC_DOWN, KC_RGHT}
+      {KC_LCTL,     TG(3),   KC_LGUI, KC_LALT, MO(4),   KC_SPC,  KC_SPC,  KC_NO,   KC_LEFT, KC_UP,   KC_DOWN, KC_RGHT}
     },
     [_ZZ] = { /* LAYER 4: RESET LAYER */
       {M(2),    M(2),    M(2),    M(2),    M(2),    M(2),    M(2),    M(2),    M(2),    M(2),    M(2),   M(2) },
       {M(2),    M(2),    M(2),    M(2),    M(2),    M(2),    M(2),    M(2),    M(2),    M(2),    M(2),   M(2) },
       {M(2),    M(2),    M(2),    M(2),    M(2),    M(2),    M(2),    M(2),    M(2),    M(2),    M(2),   M(2) },
-      {M(2),    M(2),    M(2),    M(2),    M(2),    M(2),    M(2),    TG(4),   M(2),    M(2),    M(2),   RESET}
+      {M(2),    M(2),    M(2),    M(2),    KC_TRNS, M(2),    M(2),    M(2),    M(2),    M(2),    M(2),   RESET}
     }
 };
 
@@ -69,9 +69,9 @@ const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt) {
           }
           break;
 
-        case 1: /* Macro: Set up empty anon func in js */
+        case 1: /* Macro: Types out git status (was having issues w/other macro getting the parens to come through) */
           if (record->event.pressed) {
-              return MACRO( I(25), T(F), T(U), T(N), T(C), T(T), T(I), T(O), T(N), T(LPRN), T(RPRN), T(SPC), T(LCBR), T(RCBR), T(MS_L), W(100), END );
+              return MACRO( I(25), T(G), T(I), T(T), T(SPC), T(S), T(T), T(A), T(T), T(U), T(S), W(50), END );
           }
           break;
 
